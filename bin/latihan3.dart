7c990719425ad7b53fb1927b9e844d7313394115
ImmanuelTan <immanuel1828@gmail.com>
2022-05-29 15:30:43 +0700

    Belajar Variable

---
+++ bin/latihan3.dart
@@ -0,0 +1,26 @@
+import 'package:latihan3/latihan3.dart' as latihan3;
+
+void main(List<String> arguments) {
+  // deklarasi variable String
+  var greetings = "Hello Dart 1 ";
+  /* print(variable)
+     menampilkan variable
+  */
+  print(greetings);
+  /// menampilkan string
+  print("Hello Dart 2");
+  /// deklarasi variabel integer
+  var myAges= 20;
+  /// menampilkan variable
+  print(myAges);
+  /// menampilkan variable string, string dan variable integer
+  print (greetings+"usia $myAges");
+  /// deklarasi variable dan value secara terpisah
+  var myAges2;
+  myAges2 = 20;
+  print(myAges2);
+  /// deklarasi variable null
+  var nul;
+  nul;
+  print("$nul kosong ");
+}
---
+++ test/latihan3_test.dart
@@ -0,0 +1,8 @@
+import 'package:latihan3/latihan3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
